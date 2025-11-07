dc0fe6383e735d0c64b229c5a7b4fed7a1c960e5
João Pellacani <eng.joaopellacani@gmail.com>
2025-11-07 16:44:23 -0300

    Linear Regressor plot and metrics

--- src/model.py
+++ src/model.py
@@ -1,4 +1,47 @@
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import numpy as np
 
-def cross_val_score_features(n_splits, random_state, input, target):
-    pass
+# Condfigurando o KFold para o cross validation
+def kfold_model (n_splits, random_state):
+    cv = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
+    return cv
+
+# Utilizando LInera Regression
+def linearRegression_model(input, target, cv, scoring: str):
+    model = LinearRegression()
+    param_grid = {}  # LinearRegression não tem hiperparâmetros relevantes aqui
+    grid_search = GridSearchCV(model, param_grid, cv=cv, scoring=scoring)
+    grid_search.fit(input, target)
+
+    preds = grid_search.predict(input)
+    
+    metrics = {
+        "R2": r2_score(target, preds),
+        "MAE": mean_absolute_error(target, preds),
+        "MSE": mean_squared_error(target, preds),
+        "RMSE": np.sqrt(mean_squared_error(target, preds))
+    }
+    
+    return metrics, preds, target
+
+# Utilizando LInera Regression
+def randomForest_model(input, target, cv, scoring: str):
+    model = RandomForestRegressor()
+    param_grid = {}  # LinearRegression não tem hiperparâmetros relevantes aqui
+    grid_search = GridSearchCV(model, param_grid, cv=cv, scoring=scoring)
+    grid_search.fit(input, target)
+
+    preds = grid_search.predict(input)
+    
+    metrics = {
+        "R2": r2_score(target, preds),
+        "MAE": mean_absolute_error(target, preds),
+        "MSE": mean_squared_error(target, preds),
+        "RMSE": np.sqrt(mean_squared_error(target, preds))
+    }
+    
+    return metrics, preds, target
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 def plot_eda(df, year, figures_path='../reports/figures', data_path='../data/processed'):
 
@@ -37,3 +38,14 @@ def plot_eda(df, year, figures_path='../reports/figures', data_path='../data/pro
         print(f"Arquivo EDA gerado e salvo para {year} em:")
         print(f' -> {figures_path}/eda_{year}.png')
         print(f' -> {data_path}/AEP_{year}.csv')
+
+# Plot Gráfico de avaliação de modelo de regressão
+def plot_regressor_model_ultils(pred, target):
+        df_temp = pd.DataFrame({'Desejado': target, 'Estimado': pred}) # Criação de um dataframe com os dados desejados e os estimados na predição
+        df_temp = df_temp.head(60) # Armazena a quantidade de elementos a serem apresentados no gráfico, pois pode ser visualmente difícil de abstrair caso tenham muitas informações
+        df_temp.plot(kind='bar',figsize=(10,6)) # Configuração do tipo de gráfico 'bar' e tamanho da figura
+        plt.grid(which='major', linestyle='-', linewidth='0.5', color='gray') # Configuração do grid do gráfico
+        plt.grid(which='minor', linestyle=':', linewidth='0.5', color='blue') # Configuração do grid do gráfico
+        plt.show() # Apresenta o gráfico comparando o desejado e o estimado pelo modelo neural
+
+        return
